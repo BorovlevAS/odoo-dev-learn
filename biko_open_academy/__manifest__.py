c95f8cdd3b71e50788825b9b6ed794c0b553a405
Artem Borovlev <bas@qdes.com.ua>
2022-03-21 22:47:28 +0200

    menus

--- biko_open_academy/__manifest__.py
+++ biko_open_academy/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "BIKO: open academy 15.0.3",
+    'name': "BIKO: open academy 15.0.4",
 
     'summary': """
         Тестовый модуль
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '15.0.3',
+    'version': '15.0.4',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
