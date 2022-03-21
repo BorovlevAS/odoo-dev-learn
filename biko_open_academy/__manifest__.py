677c1c1ca89303ec37a29409591dacfce883d6ae
Artem Borovlev <bas@qdes.com.ua>
2022-03-21 20:42:34 +0200

    basic changes to model

--- biko_open_academy/__manifest__.py
+++ biko_open_academy/__manifest__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "BIKO_open_academy",
+    'name': "BIKO: open academy 15.0.1",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Тестовый модуль
+        """,
 
     'description': """
-        Long description of module's purpose
+        Обучение по разработке модулей для ODOO
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "BIKO",
+    'website': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '15.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
