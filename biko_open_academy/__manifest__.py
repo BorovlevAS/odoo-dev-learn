8d9b0e40a2ebd3b869d21990aebaba7973be28de
Artem Borovlev <bas@qdes.com.ua>
2022-03-21 21:51:56 +0200

    view

--- biko_open_academy/__manifest__.py
+++ biko_open_academy/__manifest__.py
@@ -25,8 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/menus.xml',
         'views/views.xml',
+        'views/menus.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
