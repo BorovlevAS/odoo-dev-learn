28135a33b8c4372689a3123c0294ccfe1870ac6c
Artem Borovlev <bas@qdes.com.ua>
2022-03-26 01:27:23 +0200

    translation, security, wizard

---
+++ biko_open_academy/models/biko_oa_wizard.py
@@ -0,0 +1,12 @@
+from odoo import models,fields
+
+class biko_oa_wizard(models.TransientModel):
+    _name = 'biko.oa.wizard'
+    _description = 'BIKO: Open Academy Wizard'
+
+    session_ids = fields.Many2many('biko.oa.sessions')
+    attendee_ids = fields.Many2many('res.partner')
+
+    def add_atteendees_to_sessions(self):
+        for session in self.session_ids:
+            session.attendee_id += self.attendee_ids
