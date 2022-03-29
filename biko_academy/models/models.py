93875d6d845886d9640bdec28423afecef5581b3
Artem Borovlev <bas@qdes.com.ua>
2022-03-30 00:56:56 +0300

    prices

--- biko_academy/models/models.py
+++ biko_academy/models/models.py
@@ -6,12 +6,11 @@ class Teachers(models.Model):
 
     name = fields.Char(required=True)
     biography = fields.Html()
-    course_ids = fields.One2many('biko_academy.courses', 'teacher_id', string='Courses')
+    course_ids = fields.One2many('product.template', 'teacher_id', string='Courses')
 
 
 class Courses(models.Model):
-    _name = 'biko_academy.courses'
-    _inherit = ['mail.thread', 'product.template']
+    _inherit = 'product.template'
 
     name = fields.Char(required=True)
     description = fields.Char()
