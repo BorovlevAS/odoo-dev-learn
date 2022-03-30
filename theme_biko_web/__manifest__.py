08b5b576b9d7e140ca598b7ee4438ab919219150
Artem Borovlev <bas@qdes.com.ua>
2022-03-30 21:16:14 +0300

    adding new tutorial theme

---
+++ theme_biko_web/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'BIKO: tutorial theme',
+    'description': 'BIKO: tutorial theme',
+    'version': '1.0.1',
+    'author': 'BIKO',
+    'category': 'Theme/Creative',
+
+    'depends': ['website'],
+    'data': [
+    ],
+}
