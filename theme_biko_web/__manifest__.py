c41094772772055021fe7dabe141289094b4de35
Artem Borovlev <bas@qdes.com.ua>
2022-04-01 23:59:20 +0300

    snippet options

--- theme_biko_web/__manifest__.py
+++ theme_biko_web/__manifest__.py
@@ -10,14 +10,21 @@
         'views/layout.xml',
         'views/pages.xml',
         'views/snippets.xml',
+        'views/options.xml',
     ],
 
     'assets': {
+        'web.assets_frontend':[
+            'theme_biko_web/static/scss/style.scss',
+        ],
         'web._assets_primary_variables': [
             'theme_biko_web/static/scss/primary_variables.scss',
         ],
         'web._assets_frontend_helpers': [
             'theme_biko_web/static/scss/bootstrap_overriden.scss',
         ],
+        'website.assets_editor': [
+            'theme_biko_web/static/js/tutorial_editor.js',
+        ]
     }
 }
